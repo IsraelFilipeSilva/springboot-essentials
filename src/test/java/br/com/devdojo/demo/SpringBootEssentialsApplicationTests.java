620cea229e15f0761cfb208834be7945f0883b34
IsraelFilipe <israel.filipe@outlook.com>
2020-03-28 14:22:44 +0100

    Initial commit

---
+++ src/test/java/br/com/devdojo/demo/SpringBootEssentialsApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.devdojo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEssentialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
