f4c8bc2fa66bdc7dff278d45ade620ab13b96d99
IsraelFilipe <israel.filipe@outlook.com>
2020-04-05 15:16:23 +0200

    Put problem solved. 5 April edit 1.2

--- src/main/java/br/com/devdojo/demo/endpoint/StudentEndPoint.java
+++ src/main/java/br/com/devdojo/demo/endpoint/StudentEndPoint.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
@@ -42,7 +43,7 @@ public class StudentEndPoint {
 
     @PostMapping
     @Transactional
-    public ResponseEntity<?> save(@RequestBody Student student) {
+    public ResponseEntity<?> save(@Valid @RequestBody Student student) {
         return new ResponseEntity<>(studentDAO.save(student), HttpStatus.CREATED);
     }
 
